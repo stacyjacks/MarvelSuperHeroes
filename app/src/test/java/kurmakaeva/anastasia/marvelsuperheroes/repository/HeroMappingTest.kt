0dc7fb3299462aa0c765f2a6c0c16fa88553eeb1
Anastasia K <34249502+stacyjacks@users.noreply.github.com>
2022-08-01 12:21:00 +0200

    Added test for mapping function.

---
+++ app/src/test/java/kurmakaeva/anastasia/marvelsuperheroes/repository/HeroMappingTest.kt
@@ -0,0 +1,37 @@
+package kurmakaeva.anastasia.marvelsuperheroes.repository
+
+import kurmakaeva.anastasia.data.network.dtos.HeroDTO
+import kurmakaeva.anastasia.data.network.dtos.HeroThumbnailDTO
+import kurmakaeva.anastasia.data.network.dtos.HeroUrlDTO
+import kurmakaeva.anastasia.domain.repository.entities.Hero
+import kurmakaeva.anastasia.domain.repository.entities.HeroUrl
+import kurmakaeva.anastasia.domain.repository.toHero
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class HeroMappingTest {
+    private val id = 1
+    private val name = "Thor"
+    private val description = "God of Thunder"
+    private val thumbnailPath = "image"
+    private val thumbnailExtension = ".jpg"
+    private val urls = listOf<HeroUrlDTO>()
+
+    @Test
+    fun heroDtoToEntity() {
+        val heroDTO = HeroDTO(
+            id = id,
+            name = name,
+            description = description,
+            thumbnail = HeroThumbnailDTO(thumbnailPath, thumbnailExtension),
+            urls = urls
+        )
+
+        val mappedHero = heroDTO.toHero()
+
+        assertEquals(
+            Hero(id, name, thumbnailPath, thumbnailExtension, description, urls.map { HeroUrl(it.url) }),
+            mappedHero
+        )
+    }
+}
