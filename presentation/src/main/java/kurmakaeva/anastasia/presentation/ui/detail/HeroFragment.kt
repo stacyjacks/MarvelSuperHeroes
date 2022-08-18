0aed21e97c65e3d78b9d4a4f2114f1022f87c49a
Anastasia K <34249502+stacyjacks@users.noreply.github.com>
2022-08-18 15:51:35 +0200

    Improved imports.

--- presentation/src/main/java/kurmakaeva/anastasia/presentation/ui/detail/HeroFragment.kt
+++ presentation/src/main/java/kurmakaeva/anastasia/presentation/ui/detail/HeroFragment.kt
@@ -11,12 +11,18 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.ComposeView
@@ -24,7 +30,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
@@ -35,9 +40,15 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.delay
 import kurmakaeva.anastasia.domain.entities.Hero
 import kurmakaeva.anastasia.presentation.R
-import kurmakaeva.anastasia.presentation.ui.*
+import kurmakaeva.anastasia.presentation.ui.AppScaffold
+import kurmakaeva.anastasia.presentation.ui.EmptyState
+import kurmakaeva.anastasia.presentation.ui.HTTP
+import kurmakaeva.anastasia.presentation.ui.HTTPS
+import kurmakaeva.anastasia.presentation.ui.IMAGE_PATH_FANTASTIC
+import kurmakaeva.anastasia.presentation.ui.LoadingIndicator
+import kurmakaeva.anastasia.presentation.ui.REFRESH_DELAY
+import kurmakaeva.anastasia.presentation.ui.ReadMoreButton
 import kurmakaeva.anastasia.presentation.ui.theme.RedPrimary
-import kurmakaeva.anastasia.presentation.ui.theme.White
 import javax.inject.Inject
 
 @AndroidEntryPoint
